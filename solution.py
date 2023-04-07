357c322981d3e33631e0dc14f6f139d179cf1175
fedorkray <130180153+fedorkray@users.noreply.github.com>
2023-04-07 23:12:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,9 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    y = np.log(abs(x - 187))
+    y_mean = np.mean(y)
+    y_var = np.var(y)
+    sigma2_hat = np.maximum(y_var - np.log(y_var/y_mean**2 + 1), 1e-6)
+    alpha_hat = np.exp(y_mean - sigma2_hat/2)
     return x.mean() # Ваш ответ
